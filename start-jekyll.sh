30ae2a44a906eec197a4b5e33888f7800b71c970
Michael McGarrah <mcgarrah@gmail.com>
2024-06-10 13:58:43 -0400

    Update the start script and add how to setup in new env

--- start-jekyll.sh
+++ start-jekyll.sh
@@ -1,1 +1,23 @@
+# Setting up on Ubuntu 22.04 LTS
+#
+# sudo apt -y install make build-essential ruby ruby-dev
+# echo "" >> $HOME/.bashrc
+# echo "# Ruby Jekyll Gems" >> $HOME/.bashrc
+# echo "if [ ! -d $HOME/gems ]; then" >> $HOME/.bashrc
+# echo "  mkdir $HOME/gems" >> $HOME/.bashrc"
+# echo "fi" >> $HOME/.bashrc"
+# echo "export GEM_HOME=$HOME/gems" >> $HOME/.bashrc
+# echo "export PATH=$HOME/gems/bin:$PATH" >> $HOME/.bashrc
+# source $HOME/.bashrc
+# gem install jekyll bundler
+
+# VS Code Extension
+#
+# Name: Jekyll Run
+# Id: Dedsec727.jekyll-run
+# Description: Build and Run your Jekyll static website
+# Version: 1.7.0
+# Publisher: Dedsec727
+# VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=Dedsec727.jekyll-run
+
 bundle exec jekyll serve
